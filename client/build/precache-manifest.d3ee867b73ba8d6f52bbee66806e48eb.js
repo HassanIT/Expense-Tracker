252742c32f28bd0b15841e0e2540155ee7728db4
Hassan <hassan8b2000@gmail.com>
2020-08-03 22:54:33 -0400

    My initial commit

---
+++ client/build/precache-manifest.d3ee867b73ba8d6f52bbee66806e48eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b82952b8d26356fe69b5673f8ef2ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680f7aadce220d50b0b3",
+    "url": "/static/css/main.5d96a794.chunk.css"
+  },
+  {
+    "revision": "aefcadf35b12fe9f2208",
+    "url": "/static/js/2.e85e92f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e85e92f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680f7aadce220d50b0b3",
+    "url": "/static/js/main.c414dbaf.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
